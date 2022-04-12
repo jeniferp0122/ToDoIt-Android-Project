5e1d0fc4b89674ba7d21c43df89af195b54c3f49
ALAIN A PEREZ <alain.perez@dcodebox.com>
2022-04-11 20:40:37 -0400

    Linked Main Activity Buttons to Login and Signup

--- app/src/main/java/com/example/todoit/MainActivity.java
+++ app/src/main/java/com/example/todoit/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.todoit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goLoginActivity();
+                Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_SHORT);
+            }
+        });
+
+
     }
+    private void goLoginActivity(){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
+
 }
