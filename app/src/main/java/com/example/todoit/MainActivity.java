f1ee3953727b857f5d3c8aa13f5d3544463a7e89
ALAIN A PEREZ <alain.perez@dcodebox.com>
2022-04-11 21:08:29 -0400

    Functional Buttons on Main Screen

--- app/src/main/java/com/example/todoit/MainActivity.java
+++ app/src/main/java/com/example/todoit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG , "Main Was Entered to ");
 
 
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSignUp();
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i(TAG , "onCLick was pressed");
                 goLoginActivity();
                 Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_SHORT);
             }
@@ -33,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
     }
     private void goLoginActivity(){
         Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goToSignUp(){ //moves you to the Sign Up Activity
+        Intent i = new Intent(this, SignupActivity.class);
         startActivity(i);
         finish();
     }
